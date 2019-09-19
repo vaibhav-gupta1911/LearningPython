a13ea59b5c514d4f961cbf01ba768b45bf16a4ff
vaibhav-gupta1911 <gupta.vaibhav1911@gmail.com>
2019-09-18 18:43:00 -0700

    Learning Strings and Numbers
    Learning Strings and Numbers

---
+++ Numbers.py
@@ -0,0 +1,20 @@
+from math import *
+hello = "Hallow"
+
+my_num = 19
+print(-2.33 + 1)
+print(10 % 3)  # modules/reminder
+
+print(hello + str(my_num))  # conversion
+print(abs(-2))  # absolute value
+print(pow(2, 3))  # power
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.1))
+print(round(3.5))
+
+print(floor(3.5))  # require math to be imported
+print(ceil(3.1))
+print(sqrt(36))
+
+
---
+++ Strings.py
@@ -0,0 +1,23 @@
+from math import *
+
+hello = "Hello"  # String
+day = 12  # int/float
+isExist = True  # Boolean
+
+print(hello + " \"World")  # quotation mark \"
+
+hello = "Hallow"
+print(hello + " \nWorld")  # next line \n
+
+print(hello.upper())  # upper case
+print(hello.lower())  # lower case
+print(hello.isupper())  # boolean is upper check
+print(hello.upper().isupper())  # boolean is upper check
+print(len(hello))  # get length
+print(hello[2])  # return 3rd character
+print(hello.index("l"))  # return first index of matched char
+print(hello.index("al"))  # return first index of matched char
+print(hello.replace("l", "m"))  # replace all occurrences
+
+
+
